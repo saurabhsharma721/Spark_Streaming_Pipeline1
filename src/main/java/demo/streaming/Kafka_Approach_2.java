c88b66ea29af2cb107b2d93f7f56d74c36ed36cf
saurabhsharma721 <tosaurabh85@gmail.com>
2015-09-20 21:16:09 -0500

    Update Kafka_Approach_2.java
    Changes to Make Approach 2

--- src/main/java/demo/streaming/Kafka_Approach_2.java
+++ src/main/java/demo/streaming/Kafka_Approach_2.java
@@ -8,10 +8,14 @@ import java.util.Map;
 import java.util.Set;
 
 import kafka.serializer.StringDecoder;
+import demo.streaming.Kafka_Approach_1.kafkaEventsSavetoRepoistory;
 import demo.streaming.config.JavaStreamingContextFactory;
+import demo.streaming.dao.SaveToRepository;
+import demo.streaming.mapper.MessageToTruckEventMapper;
 import demo.streaming.model.TruckEvents;
 
 import org.apache.log4j.Logger;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
@@ -20,7 +24,7 @@ import org.apache.spark.streaming.kafka.KafkaUtils;
 
 import scala.Tuple2;
 /*
- * This class is under construction. Not tested yet
+ * This cass in the preferred Approach
  * 
  */
 
@@ -31,19 +35,7 @@ public class Kafka_Approach_2 {
 
 	private static String datePattern ="yyyy-mm-dd hh:mm:ss.SSS";
 	
-	public static JavaPairInputDStream<String, String>  intializeKafkaInputDStream(JavaStreamingContext jssc,
-			Map<String,String> envParams, Set<String> topicSet){
-			return KafkaUtils.createDirectStream(
-					jssc,
-					String.class,
-					String.class,
-					StringDecoder.class,
-					StringDecoder.class,
-					envParams,
-					topicSet
-				);
-	}
-	
+		
 	public static void main(String s[]){
 		int seconds = 10;
 		logger.info("Kafka Approach 2 with Duration -" +seconds);
@@ -54,42 +46,42 @@ public class Kafka_Approach_2 {
 		topicSet.add("truckevent");
 		
 		HashMap<String, String> kafkaParams = new HashMap<String, String>();
-		kafkaParams.put("metadata.broker.list", "localhost:2181");
+		kafkaParams.put("metadata.broker.list", "localhost:9092");
 		// Create direct kafka stream with brokers and topics
-		JavaPairInputDStream<String, String> messages = intializeKafkaInputDStream(jssc,
+		JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+				jssc,
+				String.class,
+				String.class,
+				StringDecoder.class,
+				StringDecoder.class,
 				kafkaParams,
-				topicSet);
-		
-		// Get the lines, split them into words, count the words and print
-		JavaDStream<TruckEvents> truckEvents = messages.map(new Function<Tuple2<String, String>, TruckEvents>() {
-					/**
-					 * 
-					 */
-					private static final long serialVersionUID = 1L;
-
-					@Override
-					public TruckEvents call(Tuple2<String, String> tuple2) throws ParseException {
-						String truckEvents[] =tuple2._2().split("|");
-						TruckEvents tr = new TruckEvents();
-						if(truckEvents.length>0){
-							SimpleDateFormat dt = new SimpleDateFormat(datePattern);
-							tr.setEventTime(dt.parse(truckEvents[0]));
-							tr.setTruckId(truckEvents[1]);
-							tr.setTruckDriverId(truckEvents[2]);
-							tr.setEventDetail(truckEvents[3]);
-							tr.setLatitudeId(Double.parseDouble(truckEvents[4]));
-							tr.setLongitudeId(Double.parseDouble(truckEvents[5]));
-						}
-						logger.info("Message proceesed: " +tr.toString());
-						return tr;
-					}
-				});
+				topicSet
+			);
+				
+		//kafkaEventsTransformer class defined below below
+		JavaDStream<TruckEvents> truckEvents = messages.map(new MessageToTruckEventMapper.kafkaEventsTransformer()); 
 			
-		truckEvents.print(10);
+		truckEvents.foreachRDD(new kafkaEventsSavetoRepoistory());
 		jssc.start();              // Start the computation
 		jssc.awaitTermination(); 
 				
 		
 	}
+	
+	public static class kafkaEventsSavetoRepoistory implements 
+	Function<JavaRDD<TruckEvents>, Void> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -39220592115450806L;
+
+		@Override
+		public Void call(JavaRDD<TruckEvents> v1) throws Exception {
+			SaveToRepository.saveToRepository(v1);
+			return null;
+		}
+
+}
 
 }
